1a251fd0229b1bb0a9d8d13c4c695085f94c8080
aryanluffy <aryanchauhan5003@gmail.com>
2020-05-02 18:05:32 +0530

    Added hosteluser class

---
+++ app/src/main/java/com/example/hostel_app/Hosteluser.java
@@ -0,0 +1,56 @@
+package com.example.hostel_app;
+
+import androidx.annotation.NonNull;
+
+public class Hosteluser {
+    private String name,email,mobile,gender,room;
+    private int age;
+    private boolean resident;
+
+    //getters
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getRoom() {
+        return room;
+    }
+    public boolean isResident() {
+        return resident;
+    }
+
+    //setters
+    public void setResident(boolean resident) {
+        this.resident = resident;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+}
--- app/src/main/java/com/example/hostel_app/MainActivity.java
+++ app/src/main/java/com/example/hostel_app/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.hostel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button submit;
+    EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submit=findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Hosteluser temp = new Hosteluser();
+//                temp.setAge();
+                text=findViewById(R.id.email);
+                temp.setEmail(text.getText().toString());
+                text=findViewById(R.id.name);
+                temp.setName(text.getText().toString());
+                text=findViewById(R.id.mobile);
+                temp.setMobile(text.getText().toString());
+                temp.setResident(false);
+                temp.setRoom("");
+//                Toast.makeText(getApplicationContext(),temp.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
